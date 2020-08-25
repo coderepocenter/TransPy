3eef28ef5e3927394cb97559e15d611137cd1137
moe <maboualidev@gmail.com>
2020-08-25 13:09:11 -0400

    Initial Setup split_linestring function

---
+++ transpy/_version.py
@@ -0,0 +1,5 @@
+__version__major__: int = 2020
+__version__minor__: int = 8
+__version__revision__: int = 24
+
+__version__: str = f'{__version__major__}.{__version__minor__}.{__version__revision__}'
